235a597779b127aaa77bb995755daaaa318404a7
Alan Rinaldi <rinaldialan2@gmail.com>
2023-05-17 15:24:00 +0200

    fix entity

--- src/main/java/cloudmark/entity/Customer.java
+++ src/main/java/cloudmark/entity/Customer.java
@@ -7,11 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Customer {
 
     @Id
