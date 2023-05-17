153adf329fc9cce7356b41deadf5984c112d9d4f
Alan Rinaldi <rinaldialan2@gmail.com>
2023-05-17 15:20:01 +0200

    Inserita la parte del customer + app.properties

---
+++ src/main/java/cloudmark/controller/CustomerController.java
@@ -0,0 +1,48 @@
+package cloudmark.controller;
+
+import java.util.Map;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cloudmark.entity.Customer;
+import cloudmark.service.CustomerService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("api/customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("/")
+    public Customer saveCustomer(@Valid @RequestBody Customer customer){
+        return customerService.saveCustomer(customer);
+    }
+
+    @PutMapping("/")
+    public Customer updateCustomer(@Valid @RequestBody Customer customer){
+        return customerService.updateCustomer(customer);
+    }
+
+    @DeleteMapping("/id/{customerId}")
+    public Map<Boolean,String> deleteCustomer(@PathVariable Integer customerId){
+        return customerService.deleteCustomer(customerId);
+    }
+
+    @GetMapping("/")
+    public List<Customer> findAll(){
+        return customerService.findAll();
+    }
+    
+
+}
