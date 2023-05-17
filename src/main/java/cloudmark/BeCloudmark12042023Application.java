d5f9e9b57d7b3b0852c004cb50bff2a014cc9458
abSimone <s.angelonibei@digitality-consulting.com>
2023-05-17 11:04:20 +0200

    progetto

---
+++ src/main/java/cloudmark/BeCloudmark12042023Application.java
@@ -0,0 +1,13 @@
+package cloudmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCloudmark12042023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCloudmark12042023Application.class, args);
+	}
+
+}
