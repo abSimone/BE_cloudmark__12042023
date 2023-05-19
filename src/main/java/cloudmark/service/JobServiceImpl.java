663472cd0b2effafbe08122f912598dd6dd8d476
Francesco <iudica.francesco@gmail.com>
2023-05-19 11:25:21 +0200

    adeguati insert e update job e employee

--- src/main/java/cloudmark/service/JobServiceImpl.java
+++ src/main/java/cloudmark/service/JobServiceImpl.java
@@ -11,35 +11,50 @@ import cloudmark.entity.Job;
 import cloudmark.repository.JobRepository;
 
 @Service
-public class JobServiceImpl implements JobService{
+public class JobServiceImpl implements JobService {
 
     @Autowired
     private JobRepository jobRepository;
 
+    /*
+     * abbiamo definito che i job vengono create e che solo
+     * successivamente
+     * vi si associano dei employees. Ovvero non si verifica il caso in cui si
+     * inserisca una job con dei employees associati che non sono presenti su db;
+     * Inoltre il customer  della commessa deve essere già presente sul db ovviamente.
+     */
     @Override
     public Job saveJob(Job job) {
-        return jobRepository.save(job);
+        if (job != null) {
+            return jobRepository.save(job);
+        } else {
+            // risposta da sistemare con le eccezioni
+            throw new IllegalArgumentException("Job cannot be null");
+        }
     }
 
     @Override
     public Job updateJob(Job job) {
-        return jobRepository.save(job);
+        if (job != null) {
+            return jobRepository.save(job);
+        } else {
+            throw new IllegalArgumentException("Job cannot be null");
+        }
+
     }
 
     @Override
     public Map<Boolean, String> deleteJob(int id) {
-        Map<Boolean,String> deleteMap=new HashMap<>();
+        Map<Boolean, String> deleteMap = new HashMap<>();
 
-        if(jobRepository.existsById(id)){
-            try{
+        if (jobRepository.existsById(id)) {
+            try {
                 jobRepository.deleteById(id);
                 deleteMap.put(true, "deleteJob success");
-            }
-            catch(IllegalArgumentException e ){
+            } catch (IllegalArgumentException e) {
                 deleteMap.put(false, "deleteJob error");
             }
-        }
-        else{
+        } else {
             deleteMap.put(false, "job not exist");
         }
         return deleteMap;
@@ -49,5 +64,5 @@ public class JobServiceImpl implements JobService{
     public List<Job> findAll() {
         return jobRepository.findAll();
     }
-    
+
 }
