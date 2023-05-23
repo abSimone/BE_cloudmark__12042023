5ea64668d441bde8df0cb156a1cd403d1c8a7670
robert-bugean <robert.bugean01@gmail.com>
2023-05-23 10:11:10 +0200

    fix RecordNotFoundException

--- src/main/java/cloudmark/exception/RecordNotFoundException.java
+++ src/main/java/cloudmark/exception/RecordNotFoundException.java
@@ -12,9 +12,14 @@ public class RecordNotFoundException extends RuntimeException {
 
     Map<String,String> errors = new HashMap<>();
 
-    public RecordNotFoundException(String message, String recordType, String error) {
+    public RecordNotFoundException(String message, String fieldName, String error) {
         super(message);
-        this.errors.put(recordType, error);
+        this.errors.put(fieldName, error);
+    }
+
+    public RecordNotFoundException(String message, Map<String,String> errors) {
+        super(message);
+        this.errors = errors;
     }
 
 }
