a31547cf566187761cf81ea0b03b4b0504270adf
robert-bugean <robert.bugean01@gmail.com>
2023-06-12 11:10:42 +0200

    firefox fix

--- src/main/java/cloudmark/controller/CustomerController.java
+++ src/main/java/cloudmark/controller/CustomerController.java
@@ -3,7 +3,6 @@ package cloudmark.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +17,6 @@ import cloudmark.service.CustomerService;
 import jakarta.validation.Valid;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/customer")
 public class CustomerController {
--- src/main/java/cloudmark/controller/EmployeeController.java
+++ src/main/java/cloudmark/controller/EmployeeController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/employee")
 public class EmployeeController {
